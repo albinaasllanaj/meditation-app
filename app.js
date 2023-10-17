fea430f5d0f59a3803e691060235e5c63f682e3e
Albina Asllanaj <112036955+albinaasllanaj@users.noreply.github.com>
2023-10-16 21:51:24 -0400

    added js for the play/pause button and background animation

---
+++ app.js
@@ -0,0 +1,41 @@
+const app = () => {
+  const song = document.querySelector(".song");
+  const play = document.querySelector(".play");
+  const outline = document.querySelector(".moving-outline circle");
+  const video = document.querySelector(".vid-container video");
+
+  //Sounds
+  const sounds = document.querySelectorAll(".sound-picker button");
+
+  //Time Display
+  const timeDisplay = document.querySelector(".time-display");
+
+  //get the length of the player outline
+  const outlineLength = outline.getTotalLength();
+
+  //Duration
+  let fakeDuration = 600;
+
+  outline.style.strokeDasharray = outlineLength;
+  outline.style.strokeDashoffset = outlineLength;
+
+  //play sound
+  play.addEventListener("click", () => {
+    checkPlaying(song);
+  });
+
+  //Create a function specific to stop and play the sounds
+  const checkPlaying = (song) => {
+    if (song.paused) {
+      song.play();
+      video.play();
+      play.src = "svg/pause.svg";
+    } else {
+      song.pause();
+      video.pause();
+      play.src = "svg/play.svg";
+    }
+  };
+};
+
+app();
